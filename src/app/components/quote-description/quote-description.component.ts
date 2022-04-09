afc20533c38bcbcbf37a912080313178daece7ec
MutheuLinet <muneenilinet@gmail.com>
2022-04-09 13:33:40 +0300

    Add upvote and downvote details to goal description component

--- src/app/components/quote-description/quote-description.component.ts
+++ src/app/components/quote-description/quote-description.component.ts
@@ -15,7 +15,15 @@ export class QuoteDescriptionComponent implements OnInit {
   quoteDelete(complete:boolean){
     this.isComplete.emit(complete);
   }
+  @Output() isUpVote = new EventEmitter<boolean>();
+  @Output() isDownVote = new EventEmitter<boolean>();
 
+  upVote(like:boolean){
+    this.isUpVote.emit(like);
+  }
+  downVote(dislike:boolean){
+    this.isDownVote.emit(dislike);
+  }
   constructor() { }
 
   ngOnInit(): void {
