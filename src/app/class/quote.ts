daa5927c6d8cc023142068b6ed5ac4bb39fd09ef
MutheuLinet <muneenilinet@gmail.com>
2022-04-09 13:28:36 +0300

    Add upvote and downvote to the qoute contrsuctor

--- src/app/class/quote.ts
+++ src/app/class/quote.ts
@@ -2,7 +2,7 @@ export class Quote {
     
     'showDescription': boolean;
 
-    constructor(public id: number, public quote: string, public quoteAuthor: string, public submittedBy: string, public completeDate: Date){
+    constructor(public id: number, public quote: string, public quoteAuthor: string, public submittedBy: string, public completeDate: Date,public upVote: number, public downVote: number, public isInspirational: boolean){
       this.showDescription=false;
     }
   
