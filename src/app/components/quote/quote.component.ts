b79c582d93ad5dd858b5f961e51cb9bee1be5c90
MutheuLinet <muneenilinet@gmail.com>
2022-04-09 13:32:39 +0300

    Add upvote, downvote and inspirational details to the array

--- src/app/components/quote/quote.component.ts
+++ src/app/components/quote/quote.component.ts
@@ -9,16 +9,42 @@ import { Quote } from 'src/app/class/quote';
 export class QuoteComponent implements OnInit {
   
   quotes: Quote[] = [
-    new Quote(1, 'The journey of a thousand miles begins with one step.', 'Lao Tzu', 'Lyn Mutheu', new Date(2019,9,14)),
-    new Quote(2,'That which does not kill us makes us stronger.','Friedrich Nietzsche','Lyn Mutheu',new Date(2019,6,9)),
-    new Quote(3,'You must be the change you wish to see in the world.','Mahatma Gandhi','Lyn Mutheu',new Date(2019,1,12)),
-    new Quote(4,'Sometimes my genious is almost frightening.','Samwel Chege','Lyn Mutheu',new Date(2019,11,18)),
-    new Quote(5,'Those who dare to fail miserably can achieve greatly.','Lyn Mutheu','Lyn Mutheu', new Date(2019,2,14)),
+    new Quote(1, 'The journey of a thousand miles begins with one step.', 'Lao Tzu', 'Lyn Mutheu', new Date(2019,9,14),0 , 0,false),
+    new Quote(2,'That which does not kill us makes us stronger.','Friedrich Nietzsche','Lyn Mutheu',new Date(2019,6,9),0 , 0,false),
+    new Quote(3,'You must be the change you wish to see in the world.','Mahatma Gandhi','Lyn Mutheu',new Date(2019,1,12),0 , 0,false),
+    new Quote(4,'Sometimes my genious is almost frightening.','Samwel Chege','Lyn Mutheu',new Date(2019,11,18),0 , 0,false),
+    new Quote(5,'Those who dare to fail miserably can achieve greatly.','Lyn Mutheu','Lyn Mutheu', new Date(2019,2,14),0 , 0,false),
   ];
 
   toggleDetails(index: number){
     this.quotes[index].showDescription = !this.quotes[index].showDescription;
   }  
+    upVote(isUpVote:any, index:any){
+      
+      this.quotes[index].upVote++
+      this.inspirationalQuote();
+    
+  }
+  downVote(isDownVote:any, index:any){
+    
+      this.quotes[index].downVote++
+    
+  }
+  inspirationalQuote(){
+    let maxValue: number = Math.max.apply(
+      Math,
+      this.quotes.map(function (quote){
+        return quote.upVote;
+      })
+    );
+    let quoteIndex: number = this.quotes.findIndex(
+      (quote) =>quote.upVote === maxValue
+    );
+    this.quotes.forEach((quote) =>{
+      quote.isInspirational = false;
+    });
+    this.quotes[quoteIndex].isInspirational = true;
+  }
 
   deleteQuote(isComplete:boolean, index:number){
     if(isComplete){
