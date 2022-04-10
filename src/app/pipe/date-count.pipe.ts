480a9c11f0a50d6baff6f5c64c185f95066c4222
MutheuLinet <muneenilinet@gmail.com>
2022-04-10 22:53:49 +0300

    Spacing edits

--- src/app/pipe/date-count.pipe.ts
+++ src/app/pipe/date-count.pipe.ts
@@ -7,14 +7,14 @@ export class DateCountPipe implements PipeTransform {
 
   transform(value: any): string{
     let dateCount: string= '';
-    let today: any= new Date();
+    let today: any= new Date(); 
     let difference = Math.abs(value - today) / 1000;
     let days = Math.floor(difference/86400);//60secs*60mins *24hrs in a day
     if (days > 0){
-      dateCount += Math.floor(days) + "days";
+      dateCount += Math.floor(days) + " days";
       return dateCount;
     } else {(Math.floor(days) == 0)
-      return dateCount+'0days';
+      return dateCount+'0 days';
     }
     
     
