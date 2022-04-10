1eb3d37ce7e956b2e007ac696be153f8f13daac4
MutheuLinet <muneenilinet@gmail.com>
2022-04-10 17:55:43 +0300

    Add else statement

--- src/app/pipe/date-count.pipe.ts
+++ src/app/pipe/date-count.pipe.ts
@@ -8,15 +8,16 @@ export class DateCountPipe implements PipeTransform {
   transform(value: any): string{
     let dateCount: string= '';
     let today: any= new Date();
-    var difference = Math.abs(value - today) / 1000;
-    var days = Math.floor(difference/86400);//60secs*60mins *24hrs in a day
+    let difference = Math.abs(value - today) / 1000;
+    let days = Math.floor(difference/86400);//60secs*60mins *24hrs in a day
     if (days > 0){
       dateCount += Math.floor(days) + "days";
-
+      return dateCount;
+    } else {(Math.floor(days) == 0)
+      return dateCount+'0days';
     }
     
-
-    return dateCount;
+    
   }
   
 
